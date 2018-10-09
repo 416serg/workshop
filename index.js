17651752552cb722326f2dd62bd10cc9855fc202
Matthew Mihok <4d.69.68.6f.6b@gmail.com>
2018-10-09 17:42:52 -0400

    Fixing step 2

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const { execSync } = require('child_process');
+const path = require('path');
+const { exec, execSync, spawn } = require('child_process');
+
+const key = process.env.KEY || '~/.ssh/id_rsa';
+const args = process.argv.slice(2);
+
+console.log('INFO', args);
 
 // Process Snapshot 
 // console.log('INFO', execSync('ps', { encoding: 'ascii' }));
@@ -6,3 +12,58 @@ const { execSync } = require('child_process');
 // Network Interfaces
 // console.log('INFO', execSync('ifconfig', { encoding: 'ascii' }));
 
+switch (args[0]) {
+  case 'deploy':
+    let cmdIndex = 0;
+    // Spawn a new process and run a command
+    const deploy = spawn(
+      'ssh',
+      ['-tt', '-i', key, 'root@142.93.152.46'],
+      { encoding: 'ascii' },
+    );
+
+    // process.stdin.resume();
+    // process.stdin.on('data', function (chunk) {
+    //   deploy.stdin.write(chunk);
+    // });
+
+    // Deploy server standard output event hook
+    deploy.stdout.on('data', (data) => {
+      console.log(`${data}`);
+    });
+
+    // Deploy server error output event hook
+    deploy.stderr.on('data', (data) => {
+      console.log(`ERROR ${data}`);
+    });
+    break;
+  case 'run':
+  default:
+    // Run our App
+    const app = exec(
+
+      // The command we would like to execute
+      `node ${path.join(__dirname, './src/server.js')}`,
+      
+      // Options, but specifically lets define the output as ascii rather than
+      //  the default buffer
+      { encoding: 'ascii' }, 
+     
+      // The callback
+      function () {
+        console.log('SUCCESS');
+      },
+  );
+
+    // Standard Out
+    // 
+    app.stdout.on('data', (data) => {
+      console.log(`STDOUT ${data}`);
+    });
+
+    // Standard Error
+    app.stderr.on('data', (data) => {
+      console.error(`STDERR ${data}`);
+    });
+
+}
