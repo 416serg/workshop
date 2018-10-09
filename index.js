1d0300472955ae1a9eff248945baaf6bc2884da0
Matthew Mihok <4d.69.68.6f.6b@gmail.com>
2018-10-09 17:39:53 -0400

    Step 1

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-const { execSync } = require('child_process');
+const path = require('path');
+const { spawn, execSync, exec } = require('child_process');
 
 // Process Snapshot 
-// console.log('INFO', execSync('ps', { encoding: 'ascii' }));
+console.log('INFO', execSync('ps', { encoding: 'ascii' }));
 
 // Network Interfaces
-// console.log('INFO', execSync('ifconfig', { encoding: 'ascii' }));
+console.log('INFO', execSync('ifconfig', { encoding: 'ascii' }));
 
+switch (args[0]) {
+  case 'run':
+  default:
+    // Run our App
+    const app = exec(
+
+      // The command we would like to execute
+      `node ${path.join(__dirname, './src/server.js')}`,
+      
+      // Options, but specifically lets define the output as ascii rather than
+      //  the default buffer
+      { encoding: 'ascii' }, 
+     
+      // The callback
+      function () {
+        console.log('SUCCESS');
+      },
+  );
+
+    // Standard Out
+    // 
+    app.stdout.on('data', (data) => {
+      console.log(`STDOUT ${data}`);
+    });
+
+    // Standard Error
+    app.stderr.on('data', (data) => {
+      console.error(`STDERR ${data}`);
+    });
+}
